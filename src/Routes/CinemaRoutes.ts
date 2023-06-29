3a9d700ad8e86321e1c6a6e7e7b41f3f2cb1a043
Atharva3008 <atharvapdeshpande@gmail.com>
2023-06-29 14:45:42 +0530

    missing import

--- src/Routes/CinemaRoutes.ts
+++ src/Routes/CinemaRoutes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { CinemaController } from '../controllers/CinemaController';
+import { app } from '../Utils/server';
 
 const router = express.Router();
-const cinemaController = new CinemaController();
+const cinemaController = new CinemaController(app);
 
 router.post('/', cinemaController.createCinema);
 router.post('/:cinemaId/purchase/:seatNumber', cinemaController.purchaseSeat);
